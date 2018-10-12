98206476b4a65af8cef36373a61ef161d4dcd43a
adishare <adie.share@gmail.com>
2018-10-12 09:12:25 +0700

    like n unlike

--- components/content/contentComponent.js
+++ components/content/contentComponent.js
@@ -1,6 +1,15 @@
 const content = {
     template: `<div class="col-sm-7">
     <div id="maincontent">
+        <div v-if="isLoading">
+            <div class="lds-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+        </div><br>
 
         <article>
             <div class="cont">
@@ -40,15 +49,17 @@ const content = {
         <article v-for="(audio, index) in audios" :key="audio._id">
             <div class="cont">
                 <h3>{{ audio.name }}</h3>
-                <time>{{audio._id}} Likes : 8 , Unlike : 2</time>
+                <time><button class="btn btn-default" v-on:click="like(audio._id)"> <i class="fa fa-thumbs-o-up"></i>
+                        {{ likesCount }}</button>
+                        <button class="btn btn-default" v-on:click="unlike(audio._id)"> <i class="fa fa-thumbs-o-down"></i>
+                        {{ unlikesCount }}</button></time>
             </div>
             <audio class="audio" controls="controls">
                 <source type="audio/mpeg" src=''>
             </audio>
             <div class="share-buttons">
                 <div id="fb-root"></div>
-                <div class="fb-share-button" data-href=''
-                    data-layout="button_count">
+                <div class="fb-share-button" data-href='' data-layout="button_count">
                 </div>
 
                 <div id="share">
@@ -68,8 +79,8 @@ const content = {
                         <i class="fa fa-pinterest-p"></i></a>
                 </div>
             </div>
-        </article> 
-       
+        </article>
+
 
 
 
@@ -88,7 +99,15 @@ const content = {
             source: 'Audio-Enhancer App',
             media: '',
             twit: '',
+            refCount: 0,
+            isLoading: false,
 
+            liked: false,
+            submitted : false,
+            text : 'Like',
+
+            likesCount: 22,
+            unlikesCount: 17,
             audios: []
         }
     },
@@ -107,6 +126,16 @@ const content = {
         },
         openPinterest() {
             window.open(`https://pinterest.com/pin/create/bookmarklet/?media=${this.media}&url=${this.url}&is_video=false&description==${this.title}`, 'popup', 'width=600,height=700')
+        },
+
+        setLoading(isLoading) {
+            if (isLoading) {
+                this.refCount++;
+                this.isLoading = true;
+            } else if (this.refCount > 0) {
+                this.refCount--;
+                this.isLoading = (this.refCount > 0);
+            }
         },
 
 
@@ -165,11 +194,75 @@ const content = {
                 }]
 
             }]
+        },
+        // toggleLike: function (audioId) {
+        //     if (this.liked) {
+        //         this.unlike(audioId)
+        //     } else {
+        //         this.like(audioId)
+        //     }
+        // },
+
+        like: function (audioId) {
+            this.submitted = true;
+
+            axios.post('http://localhost:3000/theaudios/unlikes/', {
+                params: {
+                    id:audioId
+                }
+            })
+            .then(function (response) {
+                console.log(response)
+                this.liked = true;
+                this.submitted = false;
+                this.text = 'Unlike';
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+
+         
+        },
+
+        unlike: function (audioId) {
+            this.submitted = true;
+
+            axios.post('http://localhost:3000/theaudios/unlikes/', {
+                params: {
+                    id:audioId
+                }
+            })
+            .then(function (response) {
+                console.log(response)
+                this.liked = false;
+                this.submitted = false;
+                this.text = 'Like';
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
         }
 
 
     },
     created() {
+        axios.interceptors.request.use((config) => {
+            this.setLoading(true);
+            return config;
+        }, (error) => {
+            this.setLoading(false);
+            return Promise.reject(error);
+        });
+
+        axios.interceptors.response.use((response) => {
+            this.setLoading(false);
+            return response;
+        }, (error) => {
+            this.setLoading(false);
+            return Promise.reject(error);
+        });
+
+
         this.audios = this.getAudios() ? this.getAudios() : []
     },
 
